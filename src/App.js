5e1c3d6baa8b799d4a0bf2fd05ad4d9d060ba512
bsonatunde <onatunde.samuel@gmail.com>
2022-11-13 15:02:25 +0100

    metabnb

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Home from './pages/Home';
+import {Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import PlaceToStay from './pages/PlaceToStay';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/places' element={<PlaceToStay />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
